c5ffd9aaa461a85d05bfa7e4fcaa7d9dc02fabdf
krushndayshmookh <krushndayshmookh@gmail.com>
2019-05-05 17:57:51 +0530

    refactor beautify a bit

--- index.js
+++ index.js
@@ -2,108 +2,99 @@
 
 // ----- Initialize Express -----
 
-var express = require('express');
-var app = express();
-
-
-
+var express = require('express')
+var app = express()
 
 // ----- Configuration -----
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000
 
-var dbport = 'mongodb://sparcuser:sparcuserpassword1@ds133762.mlab.com:33762/sparc';
+var dbport = process.env.MONGODB_URI || 'mongodb://sparcuser:sparcuserpassword1@ds133762.mlab.com:33762/sparc'
 
-var routes = require('./routes.js');
-
-
-
+var routes = require('./routes.js')
 
 // ----- Middleware -----
 
 //Import the mongoose module
-var mongoose = require('mongoose');
+var mongoose = require('mongoose')
 
 //Set up default mongoose connection
-mongoose.connect(dbport);
+mongoose.connect(dbport, { useNewUrlParser: true })
 // Get Mongoose to use the global promise library
-mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise
 //Get the default connection
-var db = mongoose.connection;
+var db = mongoose.connection
 
 //Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 // -----
 
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser')
 
 //To parse URL encoded data
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(
+	bodyParser.urlencoded({
+		extended: false
+	})
+)
 
 //To parse json data
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 // -----
 
-app.set('view engine', 'pug');
-app.set('views', './views');
+app.set('view engine', 'pug')
+app.set('views', './views')
 
 // -----
 
-var cors = require('cors');
-app.use(cors());
+var cors = require('cors')
+app.use(cors())
 
 // -----
 
-var favicon = require('serve-favicon');
-app.use(favicon('./www/favicon.ico'));
+var favicon = require('serve-favicon')
+app.use(favicon('./www/favicon.ico'))
 
 // -----
 
-app.use(express.static('www'));
+app.use(express.static('www'))
 
 // -----
 
-app.use('/', routes);
+app.use('/', routes)
 
 // -----
 
-app.use(function (req, res) {
-    res.status(404);
+app.use(function(req, res) {
+	res.status(404)
 
-    // respond with html page
-    if (req.accepts('html')) {
-        res.render('404', {
-            url: req.url
-        });
-        return;
-    }
+	// respond with html page
+	if (req.accepts('html')) {
+		res.render('404', {
+			url: req.url
+		})
+		return
+	}
 
-    // respond with json
-    if (req.accepts('json')) {
-        res.send({
-            error: 'Not found'
-        });
-        return;
-    }
+	// respond with json
+	if (req.accepts('json')) {
+		res.send({
+			error: 'Not found'
+		})
+		return
+	}
 
-    // default to plain-text. send()
-    res.type('txt').send('Not found');
-});
-
-
-
+	// default to plain-text. send()
+	res.type('txt').send('Not found')
+})
 
 // ----- Start listening -----
 
-app.listen(port, function (err) {
-
-    if (err) {
-        throw err;
-    }
-    console.log('App listening on port ' + port);
-
-});
+app.listen(port, function(err) {
+	if (err) {
+		throw err
+	}
+	console.log('App listening on port ' + port)
+})
