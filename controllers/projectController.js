c84f989f04f6772bc10be0d6cbf2f4a207844043
krushndayshmookh <krushndayshmookh@gmail.com>
2019-05-05 17:59:17 +0530

    images upload to aws

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,210 +1,237 @@
 /* eslint-env node */
 
+const fs = require('fs')
+const mime = require('mime')
+const multer = require('multer')
 
-
-var fs = require('fs');
-var mime = require('mime');
-var multer = require('multer');
-
-
+const aws = require('aws-sdk')
+const S3_BUCKET = process.env.S3_BUCKET
+aws.config.region = process.env.AWS_REGION
 
 // -----
 
-var Project = require('../models/project');
+var Project = require('../models/project')
 
 // Display list of all Projects.
-exports.project_list = function (req, res) {
-    Project.find({})
-        .exec(function (err, list_projects) {
-            if (err) {
-                throw err;
-            }
-            //Successful, so render
-            res.render('gallery', {
-                projects: list_projects
-            });
-            //res.send(list_projects);
-        });
-    //res.send('NOT IMPLEMENTED: Project list');
-};
+exports.project_list = function(req, res) {
+	Project.find({}).exec(function(err, list_projects) {
+		if (err) {
+			throw err
+		}
+		//Successful, so render
+		res.render('gallery', {
+			projects: list_projects
+		})
+		//res.send(list_projects);
+	})
+	//res.send('NOT IMPLEMENTED: Project list');
+}
 
-exports.project_edit = function (req, res) {
-    Project.find({})
-        .exec(function (err, list_projects) {
-            if (err) {
-                throw err;
-            }
-            //Successful, so render
-            res.render('edit-projects', {
+exports.project_edit = function(req, res) {
+	// Project.find({})
+	//     .exec(function (err, list_projects) {
+	//         if (err) {
+	//             throw err;
+	//         }
+	//Successful, so render
+	res.render('edit-projects' /* , {
                 projects: list_projects
-            });
-            //res.send(list_projects);
-        });
-};
+            } */)
+	//res.send(list_projects);
+	// });
+}
+
+exports.project_list_api = function(req, res) {
+	Project.find({}).exec(function(err, list_projects) {
+		if (err) {
+			throw err
+		}
+		res.send(list_projects)
+	})
+}
 
 // Display detail page for a specific Project.
-exports.project_detail = function (req, res) {
-    Project.findById(req.params.id)
-        .exec(function (err, project) {
-            if (err) {
-                throw err;
-            }
-            //Successful, so render
-            //console.log(product)
-            res.send(project);
-            //res.send(list_products);
-        });
-};
-
+exports.project_detail = function(req, res) {
+	Project.findById(req.params.id).exec(function(err, project) {
+		if (err) {
+			throw err
+		}
+		//Successful, so render
+		//console.log(product)
+		res.send(project)
+		//res.send(list_products);
+	})
+}
 
 // Handle Project create on POST.
-exports.project_create_post = function (req, res) {
+exports.project_create_post = function(req, res) {
+	// Create a Book object with escaped and trimmed data.
+	var project = new Project(req.body)
 
-    // Create a Book object with escaped and trimmed data.
-    var project = new Project({});
+	// var storage = multer.diskStorage({
+	// 	destination: './uploads',
+	// 	filename: function(req, file, cb) {
+	// 		cb(null, project._id + '.' + mime.getExtension(file.mimetype))
+	// 	}
+	// })
 
+	// var upload = multer({
+	// 	storage: storage
+	// }).any()
 
-    var storage = multer.diskStorage({
-        destination: './uploads',
-        filename: function (req, file, cb) {
-            cb(null, project._id + '.' + mime.getExtension(file.mimetype));
-        }
-    });
+	// upload(req, res, function(err) {
+	// 	if (err) {
+	// 		throw err
+	// 		//return res.end('Error uploading file.');
+	// 	} else {
+	// 		//console.log(req.body);
+	// 		//console.log(req.files);
 
-    var upload = multer({
-        storage: storage
-    }).any();
+	// 		/* */
+	// 		project.name = req.body.project_name
+	// 		project.owner = req.body.project_owner
+	// 		project.description = req.body.project_description
+	// 		project.date = req.body.project_date
+	// 		project.cost = req.body.project_cost
+	// 		project.url = req.body.project_url
+	// 		project.categories = req.body.project_categories
 
-    upload(req, res, function (err) {
-        if (err) {
-            throw err;
-            //return res.end('Error uploading file.');
-        } else {
-            //console.log(req.body);
-            //console.log(req.files);
+	// 		project.image.data = fs.readFileSync(req.files[0].path)
+	// 		project.image.contentType = req.files[0].mimetype
+	// 		//console.log(product);
 
-            /* */
-            project.name = req.body.project_name;
-            project.owner = req.body.project_owner;
-            project.description = req.body.project_description;
-            project.date = req.body.project_date;
-            project.cost = req.body.project_cost;
-            project.url = req.body.project_url;
-            project.categories = req.body.project_categories;
+	project.save(function(err) {
+		if (err) {
+			throw err
+		}
+		//successful - redirect to new book record.
+        // res.redirect('/dashboard/projects')
+        res.send(project)
+        
+	})
+	// fs.unlink(req.files[0].path, function(err) {
+	// 	if (err) {
+	// 		throw err
+	// 	}
+	// })
+	// //res.end("File has been uploaded");
+	// /**/
+	// }
+	// })
 
-
-            project.image.data = fs.readFileSync(req.files[0].path);
-            project.image.contentType = req.files[0].mimetype;
-            //console.log(product);
-
-            project.save(function (err) {
-                if (err) {
-                    throw err;
-                }
-                //successful - redirect to new book record.
-                res.redirect('/dashboard/projects');
-            });
-            fs.unlink(req.files[0].path, function (err) {
-                if (err) {
-                    throw err;
-                }
-            });
-            //res.end("File has been uploaded");
-            /**/
-        }
-
-    });
-
-
-    //res.send('NOT IMPLEMENTED: Project create POST');
-};
-
+	//res.send('NOT IMPLEMENTED: Project create POST');
+}
 
 // Handle Project delete on POST.
-exports.project_delete_post = function (req, res) {
-    Project.findByIdAndRemove(req.params.id, function (err) {
-        if (err) {
-            throw err;
-        }
-        // Success - go to author list
-        res.send(true);
-    });
+exports.project_delete_post = function(req, res) {
+	Project.findByIdAndRemove(req.params.id, function(err) {
+		if (err) {
+			throw err
+		}
+		// Success - go to author list
+		res.send(true)
+	})
 
-    // res.send('NOT IMPLEMENTED: Project delete POST');
-};
-
+	// res.send('NOT IMPLEMENTED: Project delete POST');
+}
 
 // Handle Project update on POST.
-exports.project_update_post = function (req, res) {
+exports.project_update_post = function(req, res) {
+	// Create a Book object with escaped and trimmed data.
+	var project = new Project(req.body)
 
-    // Create a Book object with escaped and trimmed data.
-    var project = new Project({
-        //name: req.body.product_name,
-        //description: req.body.product_description,
-        //cost: req.body.product_cost
-    });
+	Project.findByIdAndUpdate(req.params.id, project, {}, function(err) {
+		if (err) {
+			throw err
+		}
+		//successful - redirect to new book record.
+        // res.redirect('/dashboard/projects')
+        res.send(project)
+	})
 
+	// var storage = multer.diskStorage({
+	//     destination: './www/catalog/project',
+	//     filename: function (req, file, cb) {
 
-    var storage = multer.diskStorage({
-        destination: './www/catalog/project',
-        filename: function (req, file, cb) {
+	//         cb(null, req.params.id + '.' + mime.getExtension(file.mimetype));
+	//     }
+	// });
 
-            cb(null, req.params.id + '.' + mime.getExtension(file.mimetype));
-        }
-    });
+	// var upload = multer({
+	//     storage: storage
+	// }).any();
 
-    var upload = multer({
-        storage: storage
-    }).any();
+	// upload(req, res, function (err) {
+	//     if (err) {
+	//         throw err;
+	//         //return res.end('Error uploading file.');
+	//     } else {
+	//         //console.log(req.body);
+	//         //console.log(req.files);
 
-    upload(req, res, function (err) {
-        if (err) {
-            throw err;
-            //return res.end('Error uploading file.');
-        } else {
-            //console.log(req.body);
-            //console.log(req.files);
+	//         project.name = req.body.project_name;
+	//         project.owner = req.body.project_owner;
+	//         project.description = req.body.project_description;
+	//         project.date = req.body.project_date;
+	//         project.cost = req.body.project_cost;
+	//         project.url = req.body.project_url;
+	//         project._id = req.params.id;
+	//         project.categories = req.body.project_categories;
+	//         project.imagetype = mime.getExtension(req.files[0].mimetype);
+	//         //console.log(product);
 
+	//         Project.findByIdAndUpdate(req.params.id, project, {}, function (err) {
+	//             if (err) {
+	//                 throw err;
+	//             }
+	//             //successful - redirect to new book record.
+	//             res.redirect('/dashboard/projects');
+	//         });
 
-            project.name = req.body.project_name;
-            project.owner = req.body.project_owner;
-            project.description = req.body.project_description;
-            project.date = req.body.project_date;
-            project.cost = req.body.project_cost;
-            project.url = req.body.project_url;
-            project._id = req.params.id;
-            project.categories = req.body.project_categories;
-            project.imagetype = mime.getExtension(req.files[0].mimetype);
-            //console.log(product);
+	//         //res.end("File has been uploaded");
+	//     }
+	// });
 
-            Project.findByIdAndUpdate(req.params.id, project, {}, function (err) {
-                if (err) {
-                    throw err;
-                }
-                //successful - redirect to new book record.
-                res.redirect('/dashboard/projects');
-            });
-
-            //res.end("File has been uploaded");
-        }
-    });
-
-    //res.send('NOT IMPLEMENTED: Project update POST');
-};
+	//res.send('NOT IMPLEMENTED: Project update POST');
+}
 
 // Display detail image for a specific Enquiry.
-exports.project_image_get = function (req, res) {
-    Project.findById(req.params.id)
-        .exec(function (err, project) {
-            if (err) {
-                throw err;
-            }
+exports.project_image_get = function(req, res) {
+	Project.findById(req.params.id).exec(function(err, project) {
+		if (err) {
+			throw err
+		}
 
-            res.contentType(project.image.contentType);
-            res.send(project.image.data);
+		res.contentType(project.image.contentType)
+		res.send(project.image.data)
 
-            //res.send(list_products);
+		//res.send(list_products);
+	})
+	// res.send('NOT IMPLEMENTED: Enquiry detail: ' + req.params.id);
+}
 
-        });
-    // res.send('NOT IMPLEMENTED: Enquiry detail: ' + req.params.id);
-};
+exports.project_sign_s3_put_get = (req, res) => {
+	const s3 = new aws.S3()
+	const fileName = req.query.fileName
+	const fileType = req.query.fileType
+
+	const s3Params = {
+		Bucket: S3_BUCKET,
+		Key: fileName,
+		Expires: 60,
+		ContentType: fileType,
+		ACL: 'public-read'
+	}
+
+	s3.getSignedUrl('putObject', s3Params, (err, data) => {
+		if (err) {
+			console.error(err)
+			return res.status(500).send(err)
+		}
+		const returnData = {
+			signedRequest: data,
+			url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+		}
+		res.send(JSON.stringify(returnData))
+	})
+}
