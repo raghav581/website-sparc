6bf9c660abf51a8e2ac24591d3fce355b17437aa
sahilkindarle <sahilkindarle212@gmail.com>
2019-05-14 12:22:19 +0530

    delete person function updated

--- www/js/edit-about.js
+++ www/js/edit-about.js
@@ -1,48 +1,29 @@
 var personview;
 
-document.addEventListener("DOMContentLoaded", function () {
-    personview = new Vue({
-        el: "#peoplelist",
-        data: {
-            people: []
-        },
-        mounted: function () {
-            fetch("/person/list")
-                .then(function (response) {
-                    return response.json();
-                })
-                .then(function (listpersons) {
-                    personview.people = listpersons;
-                });
-        }
-    });
+document.addEventListener("DOMContentLoaded", function() {
+  personview = new Vue({
+    el: "#peoplelist",
+    data: {
+      people: []
+    },
+    mounted: function() {
+      fetch("/person/list")
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(listpersons) {
+          personview.people = listpersons;
+        });
+    }
+  });
 });
 
-
 function deleteperson(id) {
-
-    if (confirm('Delete this machine?')) {
-
-        fetch('/person/'+ id + ' /delete', {
-                method: 'POST',
-                mode: 'cors'
-            })
-            .then(function (response) {
-                return response.json()
-            })
-            .then(function (result) {
-                if (result) {
-                    M.toast({
-                        html: 'Machine deleted!'
-                    });
-                    // updateView()
-                    window.location.reload();
-                }
-
-            })
-
-        /* $.post('/api/machine/' + id + '/delete', function (result) {
-            updateView();
-        }); */
-    }
+  if (confirm("Are you sure you want to delete?")) {
+    $.post("/person/delete/" + id, function(status) {
+      if (status) {
+        document.location.reload();
+      }
+    });
+  }
 }
